5fbb82160f9e40f3e4e8ff1b60bc8a21636df801
Max Johansen <max.johansen@lifeomic.com>
2022-04-28 14:49:30 -0600

    chore: Update dynamo client to use `@aws-sdk/client-dynamodb`
    This updates the underlying dynamo client to use the new sdk from `@aws-sdk/client-dynamodb`.
    
    See:
    - https://github.com/lifeomic/video-service/pull/30

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 import * as DataLoader from 'dataloader';
-import { DynamoDB } from 'aws-sdk';
-import {
-  Key,
-  AttributeMap,
-  KeysAndAttributes,
-  BatchGetRequestMap
-} from 'aws-sdk/clients/dynamodb';
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 import {
   groupBy,
   find,
@@ -13,25 +8,25 @@ import {
   map,
   isEmpty,
   uniqWith,
-  isEqual
+  isEqual,
 } from 'lodash';
 
 export type Options = {
   readonly client: DynamoDB;
 };
 
-export type ItemToGet = {
+export type ItemToGet<T> = {
   readonly table: string;
-  readonly key: Key;
+  readonly key: Partial<T>;
 };
 
 const MAX_BATCH_SIZE = 100;
 
-export function createDataLoader(options: Options) {
+export function createDataLoader<T>(options: Options) {
   // Keep a local reference to the client so that it cannot be
   // changed by the caller after creating the data loader
   const client = options.client;
-  const loader = new DataLoader<ItemToGet, AttributeMap | null>(
+  const loader = new DataLoader<ItemToGet<T>, T | null>(
     async (itemsToGet) => {
       // DynamoDB does not allow duplicate keys in the batchGet requests
       // do de-duplicate the keys before building the request
@@ -40,44 +35,51 @@ export function createDataLoader(options: Options) {
       // Groups the items together by table into the request format that
       // DynamoDB expects
       const itemsByTable = groupBy(uniqueKeys, 'table');
-      const requestItems: BatchGetRequestMap = mapValues(
+      const requestItems = mapValues(
         itemsByTable,
-        (itemsToGet: ItemToGet[]): KeysAndAttributes => {
-          return { Keys: map(itemsToGet, 'key') };
+        (itemsToGet: ItemToGet<T>[]) => {
+          return { Keys: map(itemsToGet, 'key').map((key) => marshall(key)) };
         }
       );
 
       // Perform the batch lookup
-      const results = await client
-        .batchGetItem({ RequestItems: requestItems })
-        .promise();
+      const results = await client.batchGetItem({ RequestItems: requestItems });
 
       // Map the results from DyanmoDB into an array of results in the same
       // order as the requested items
-      return itemsToGet.map((itemToGet: ItemToGet) => {
+      return itemsToGet.map((itemToGet: ItemToGet<T>) => {
         if (results.Responses) {
-          const tableResults = results.Responses[itemToGet.table];
+          const rawTableResults = results.Responses[itemToGet.table];
 
           // Check if the item was found
-          if (tableResults) {
+          if (rawTableResults) {
+            const tableResults = rawTableResults.map((item) =>
+              unmarshall(item)
+            );
             const itemResult = find(tableResults, itemToGet.key);
-            if (itemResult) return itemResult;
+            if (itemResult) {
+              return itemResult as T;
+            }
           }
         }
 
         // If there are any unprocessed keys, treat them as failures that can
         // be retried
         if (results.UnprocessedKeys && !isEmpty(results.UnprocessedKeys)) {
-          const tableResults = results.UnprocessedKeys[itemToGet.table];
-          if (tableResults) {
-            const itemResult = find(tableResults.Keys, itemToGet.key);
+          const rawTableResults = results.UnprocessedKeys[itemToGet.table];
+          if (rawTableResults && rawTableResults.Keys) {
+            const itemResult = find(
+              rawTableResults.Keys.map((item) => unmarshall(item)),
+              itemToGet.key
+            );
             if (itemResult) return new Error('The item was not processed');
           }
         }
 
         return null;
       });
-    }, { maxBatchSize: MAX_BATCH_SIZE }
+    },
+    { maxBatchSize: MAX_BATCH_SIZE }
   );
 
   return loader;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,33 +1,35 @@
+import { BatchGetItemCommandOutput, DynamoDB } from '@aws-sdk/client-dynamodb';
+import { marshall } from '@aws-sdk/util-dynamodb';
 import { createDataLoader } from '../src';
-import { DynamoDB } from 'aws-sdk';
-import { Key, BatchGetItemOutput } from 'aws-sdk/clients/dynamodb';
 
-function createMockDynamoDBClient(result: BatchGetItemOutput): DynamoDB {
-  return ({
-    batchGetItem: jest.fn().mockImplementation(() => ({
-      promise: () => Promise.resolve(result)
-    }))
-  } as unknown) as DynamoDB;
+function createMockDynamoDBClient(
+  result: BatchGetItemCommandOutput | any
+): DynamoDB {
+  return {
+    batchGetItem: jest.fn().mockImplementation(() => Promise.resolve(result)),
+  } as unknown as DynamoDB;
 }
 
+type Key = any;
+
 test('uses BatchGetItem to get items', async () => {
-  const item1: Key = { id: { S: 'id1' } };
-  const item2: Key = { id: { S: 'id2' } };
+  const item1: Key = { id: 'id1' };
+  const item2: Key = { id: 'id2' };
 
   const client = createMockDynamoDBClient({
     Responses: {
-      table1: [item1, item2]
-    }
+      table1: [item1, item2].map((item) => marshall(item)),
+    },
   });
 
   const loader = createDataLoader({ client });
   const loading1 = loader.load({
     table: 'table1',
-    key: item1
+    key: item1,
   });
   const loading2 = loader.load({
     table: 'table1',
-    key: item2
+    key: item2,
   });
 
   const [loadedItem1, loadedItem2] = await Promise.all([loading1, loading2]);
@@ -39,22 +41,22 @@ test('uses BatchGetItem to get items', async () => {
 
 test('uses BatchGetItem to get items - 101 items', async () => {
   const itemArray: Key[] = new Array(101);
-  itemArray.fill({ id: {S: 'id'} } );
-  const items = itemArray.map((x, y) => ( { id: { S: `id${y + 1}` } } ));
+  itemArray.fill({ id: 'id' });
+  const items = itemArray.map((x, y) => ({ id: `id${y + 1}` }));
 
   const client = createMockDynamoDBClient({
     Responses: {
-      table1: items
-    }
+      table1: items.map((item) => marshall(item)),
+    },
   });
 
   const loader = createDataLoader({ client });
-  const loadings: Promise<DynamoDB.AttributeMap | null>[] = [];
+  const loadings: Promise<any | null>[] = [];
   for (const item of items) {
     loadings.push(
       loader.load({
         table: 'table1',
-        key: item
+        key: item,
       })
     );
   }
@@ -68,24 +70,24 @@ test('uses BatchGetItem to get items - 101 items', async () => {
 });
 
 test('supports multiple tables', async () => {
-  const item1: Key = { id: { S: 'id1' } };
-  const item2: Key = { id: { S: 'id2' } };
+  const item1: Key = { id: 'id1' };
+  const item2: Key = { id: 'id2' };
 
   const client = createMockDynamoDBClient({
     Responses: {
-      table1: [item1],
-      table2: [item2]
-    }
+      table1: [item1].map((item) => marshall(item)),
+      table2: [item2].map((item) => marshall(item)),
+    },
   });
 
   const loader = createDataLoader({ client });
   const loading1 = loader.load({
     table: 'table1',
-    key: item1
+    key: item1,
   });
   const loading2 = loader.load({
     table: 'table2',
-    key: item2
+    key: item2,
   });
 
   const [loadedItem1, loadedItem2] = await Promise.all([loading1, loading2]);
@@ -96,106 +98,106 @@ test('supports multiple tables', async () => {
 });
 
 test('marks unprocessed items as failures', async () => {
-  const item1: Key = { id: { S: 'id1' } };
-  const item2: Key = { id: { S: 'id2' } };
+  const item1: Key = { id: 'id1' };
+  const item2: Key = { id: 'id2' };
 
   const client = createMockDynamoDBClient({
     Responses: {
-      table1: [item1]
+      table1: [item1].map((item) => marshall(item)),
     },
     UnprocessedKeys: {
       table2: {
-        Keys: [item2]
-      }
-    }
+        Keys: [item2].map((item) => marshall(item)),
+      },
+    },
   });
 
   const loader = createDataLoader({ client });
   const loading1 = loader.load({
     table: 'table1',
-    key: item1
+    key: item1,
   });
   const loading2 = loader.load({
     table: 'table2',
-    key: item2
+    key: item2,
   });
 
   expect(await loading1).toEqual(item1);
   await expect(loading2).rejects.toMatchObject({
-    message: 'The item was not processed'
+    message: 'The item was not processed',
   });
 
   expect(client.batchGetItem).toBeCalledTimes(1);
 });
 
 test('handles a response with nothing but unprocessed items', async () => {
-  const item1: Key = { id: { S: 'id1' } };
-  const item2: Key = { id: { S: 'id2' } };
+  const item1: Key = { id: 'id1' };
+  const item2: Key = { id: 'id2' };
 
   const client = createMockDynamoDBClient({
     UnprocessedKeys: {
       table2: {
-        Keys: [item2]
+        Keys: [item2].map((item) => marshall(item)),
       },
       table1: {
-        Keys: [item1]
-      }
-    }
+        Keys: [item1].map((item) => marshall(item)),
+      },
+    },
   });
 
   const loader = createDataLoader({ client });
   const loading1 = loader.load({
     table: 'table1',
-    key: item1
+    key: item1,
   });
   const loading2 = loader.load({
     table: 'table2',
-    key: item2
+    key: item2,
   });
 
   await expect(loading1).rejects.toMatchObject({
-    message: 'The item was not processed'
+    message: 'The item was not processed',
   });
   await expect(loading2).rejects.toMatchObject({
-    message: 'The item was not processed'
+    message: 'The item was not processed',
   });
 
   expect(client.batchGetItem).toBeCalledTimes(1);
 });
 
 test('handles a response with found, unprocessed and null items', async () => {
-  const item1: Key = { id: { S: 'id1' } };
-  const item2: Key = { id: { S: 'id2' } };
-  const item3: Key = { id: { S: 'id3' } };
+  const item1: Key = { id: 'id1' };
+  const item2: Key = { id: 'id2' };
+  const item3: Key = { id: 'id3' };
 
   const client = createMockDynamoDBClient({
     Responses: {
-      table1: [item1]
+      table1: [item1].map((item) => marshall(item)),
     },
     UnprocessedKeys: {
       table1: {
-        Keys: [item2]
-      }
-    }
+        Keys: [item2].map((item) => marshall(item)),
+      },
+    },
   });
 
   const loader = createDataLoader({ client });
   const loading1 = loader.load({
     table: 'table1',
-    key: item1
+    key: item1,
   });
   const loading2 = loader.load({
     table: 'table1',
-    key: item2
+    key: item2,
   });
   const loading3 = loader.load({
     table: 'table2',
-    key: item3
+    key: item3,
   });
 
   expect(await loading1).toEqual(item1);
   await expect(loading2).rejects.toMatchObject({
-    message: 'The item was not processed'
+    message: 'The item was not processed',
   });
   expect(await loading3).toBeNull();
 
@@ -203,53 +205,53 @@ test('handles a response with found, unprocessed and null items', async () => {
 });
 
 test('handles a response with unprocessed and null items in the same table', async () => {
-  const item1: Key = { id: { S: 'id1' } };
-  const item2: Key = { id: { S: 'id2' } };
+  const item1: Key = { id: 'id1' };
+  const item2: Key = { id: 'id2' };
 
   const client = createMockDynamoDBClient({
     UnprocessedKeys: {
       table1: {
-        Keys: [item2]
-      }
-    }
+        Keys: [item2].map((item) => marshall(item)),
+      },
+    },
   });
 
   const loader = createDataLoader({ client });
   const loading1 = loader.load({
     table: 'table1',
-    key: item1
+    key: item1,
   });
   const loading2 = loader.load({
     table: 'table1',
-    key: item2
+    key: item2,
   });
 
   expect(await loading1).toBeNull();
   await expect(loading2).rejects.toMatchObject({
-    message: 'The item was not processed'
+    message: 'The item was not processed',
   });
 
   expect(client.batchGetItem).toBeCalledTimes(1);
 });
 
 test('returns null for items that do not exist', async () => {
-  const item1: Key = { id: { S: 'id1' } };
-  const item2: Key = { id: { S: 'id2' } };
+  const item1: Key = { id: 'id1' };
+  const item2: Key = { id: 'id2' };
 
   const client = createMockDynamoDBClient({
     Responses: {
-      table1: [item1]
-    }
+      table1: [item1].map((item) => marshall(item)),
+    },
   });
 
   const loader = createDataLoader({ client });
   const loading1 = loader.load({
     table: 'table1',
-    key: item1
+    key: item1,
   });
   const loading2 = loader.load({
     table: 'table1',
-    key: item2
+    key: item2,
   });
 
   const [loadedItem1, loadedItem2] = await Promise.all([loading1, loading2]);
@@ -260,22 +262,22 @@ test('returns null for items that do not exist', async () => {
 });
 
 test('Duplicate load requests for the same key are merged before the batch request', async () => {
-  const item1: Key = { id: { S: 'id1' } };
+  const item1: Key = { id: 'id1' };
 
   const client = createMockDynamoDBClient({
     Responses: {
-      table1: [item1]
-    }
+      table1: [item1].map((item) => marshall(item)),
+    },
   });
 
   const loader = createDataLoader({ client });
   const loading1 = loader.load({
     table: 'table1',
-    key: item1
+    key: item1,
   });
   const loading2 = loader.load({
     table: 'table1',
-    key: item1
+    key: item1,
   });
 
   const [loadedItem1, loadedItem2] = await Promise.all([loading1, loading2]);
@@ -284,13 +286,9 @@ test('Duplicate load requests for the same key are merged before the batch reque
   expect(client.batchGetItem).toBeCalledWith({
     RequestItems: {
       table1: {
-        Keys: [
-          {
-            id: { S: 'id1' }
-          }
-        ]
-      }
-    }
+        Keys: [marshall(item1)],
+      },
+    },
   });
 
   expect(loadedItem1).toEqual(item1);
